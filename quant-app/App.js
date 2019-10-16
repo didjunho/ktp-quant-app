4495267cdd89e9bd81e3fbbbe7346a5aa5a75255
hreeves <hreeves@umich.edu>
2019-10-16 19:41:35 -0400

    set up nav bar

--- quant-app/App.js
+++ quant-app/App.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.homeView}>
+        <Text style={styles.headingText}>Quant</Text>
+      </View>
+    );
+  }
 }
 
+class SearchScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Search!</Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Profile!</Text>
+      </View>
+    );
+  }
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	homeView: {
+		backgroundColor: '#262626',
+		flex: 1, 
+		justifyContent: 'center',
+		alignItems: 'center',
+
+	},
+	headingText: {
+		color: '#FFF',
+		fontSize: 50,
+		
+	}
+
+
+
 });
+
+const TabNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Search: SearchScreen,
+  Profile: ProfileScreen
+});
+
+export default createAppContainer(TabNavigator);
