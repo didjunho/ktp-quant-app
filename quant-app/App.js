d31fd2125afef553871fb523567375e61c819100
emasjedi <emasjedi@umich.edu>
2019-10-16 20:42:56 -0400

    added searchBar not done yet

--- quant-app/App.js
+++ quant-app/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { SearchBar } from 'react-native-elements';
 
 class HomeScreen extends React.Component {
   render() {
@@ -14,10 +15,28 @@ class HomeScreen extends React.Component {
 }
 
 class SearchScreen extends React.Component {
+  state = {
+    search: '',
+  };
+  updateSearch = search => {
+    this.setState({ search });
+  };
   render() {
+    const { search } = this.state;
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Search!</Text>
+      <View style={styles.homeView}>
+        <Text style={styles.headingText}>Search</Text>
+        <SearchBar
+        inputContainerStyle={{backgroundColor: '#686868'}}
+        inputStyle={{color: '#A8A8A8'}}
+        containerStyle={{backgroundColor: 'transparent', width: 250, borderTopColor: 'transparent', borderBottomColor: 'transparent'}}
+        placeholder={'Pritish Vaidya'}
+        pla
+        onChangeText={this.updateSearch}
+        value={search}
+        round={true}
+        placeholderTextColor={'#A8A8A8'}
+      />
       </View>
     );
   }
